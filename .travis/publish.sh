373559ffd33c0b7d5a21ec7a3887a8d6c022a542
Jon Brouse <jbrouse19@gmail.com>
2017-07-02 23:57:38 -0400

    Updated username/email in build script

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -15,19 +15,19 @@ update_ice_version() {
   timestamp Updating Dockerfile and commiting to master...
   sed -i "/ENV\ ICE_VERSION/c\ENV\ ICE_VERSION\ $NEW_VERSION" ice/Dockerfile
   git commit ice/Dockerfile -m "Created new release: $NEW_VERSION"
-  git push
 }
 
-create_release_tag() {
+push_to_git() {
   timestamp Creating release tag...
   sed -i "/ENV\ ICE_VERSION/c\ENV\ ICE_VERSION\ $NEW_VERSION" ice/Dockerfile
   git tag -m "New version of ICE" "$NEW_VERSION.0"
   git remote rm origin
   git remote add origin https://$GH_TOKEN@github.com/jonbrouse/docker-ice.git > /dev/null 2>&1
   git push --quiet --set-upstream origin
+  git push --quiet --set-upstream origin master
   git push --tags
 }
 
 setup_git
 update_ice_version
-create_release_tag
+push_to_git
