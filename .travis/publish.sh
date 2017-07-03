de717f7cc11c60f9cfe032a99b8c5f329429d359
Jon Brouse <jbrouse19@gmail.com>
2017-07-03 09:19:54 -0400

    More build script clean up

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -9,12 +9,14 @@ setup_git() {
   git config --global push.default matching
   git config --global user.email "jbrouse19@gmail.com"
   git config --global user.name "Jon Brouse"
+  git remote rm origin
+  git remote add origin https://$GH_TOKEN@github.com/jonbrouse/docker-ice.git > /dev/null 2>&1
 }
 
 update_ice_version() {
   timestamp Updating Dockerfile and commiting...
   sed -i "/ENV\ ICE_VERSION/c\ENV\ ICE_VERSION\ $NEW_VERSION" ice/Dockerfile
-  git commit ice/Dockerfile -m "Created new release: $NEW_VERSION"
+  git commit ice/Dockerfile -m "Updated Ice version to $NEW_VERSION"
 }
 
 update_master() {
@@ -27,8 +29,6 @@ update_master() {
 create_new_tag() {
   timestamp Creating release tag...
   git tag -m "New version of ICE" "$NEW_VERSION.0"
-  git remote rm origin
-  git remote add origin https://$GH_TOKEN@github.com/jonbrouse/docker-ice.git > /dev/null 2>&1
   git push --quiet --set-upstream origin
   git push --tags
 }
