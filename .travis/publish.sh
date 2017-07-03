1621849203b78ff30deb87e7ed9fab47cd5bcd5c
Jon Brouse <jbrouse19@gmail.com>
2017-07-02 23:15:59 -0400

    Added logging and fixed master push to travis job

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 setup_git() {
+  echo "Setting up git config..."
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
-update_version() {
+update_ice_version() {
+  echo "Updating Dockerfile and commiting to master..."
   sed -i "/ENV\ ICE_VERSION/c\ENV\ ICE_VERSION\ $NEW_VERSION" ice/Dockerfile
   git commit ice/Dockerfile -m "Created new release: $NEW_VERSION"
-  git tag -m "New version of ICE" "$NEW_VERSION.0"
+  git push
 }
 
-upload_files() {
+create_release_tag() {
+  echo "Creating release tag..."
+  sed -i "/ENV\ ICE_VERSION/c\ENV\ ICE_VERSION\ $NEW_VERSION" ice/Dockerfile
+  git tag -m "New version of ICE" "$NEW_VERSION.0"
   git remote rm origin
   git remote add origin https://$GH_TOKEN@github.com/jonbrouse/docker-ice.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
+  git push --quiet --set-upstream origin
   git push --tags
 }
 
 setup_git
-update_version
-upload_files
+update_ice_version
+create_release_tag
