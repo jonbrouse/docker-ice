b9acf151a99d05c3d79a352609dac5d06f98a4b0
Jon Brouse <jbrouse19@gmail.com>
2017-07-02 23:45:58 -0400

    Updated logging in build script

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+timestamp() {
+  echo $(date +%b\ %d\ %T) $*
+}
+
 setup_git() {
-  echo "Setting up git config..."
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  timestamp Setting up git config...
+  git config --global push.default matching
+  #git config --global user.email "travis@travis-ci.org"
+  #git config --global user.name "Travis CI"
 }
 
 update_ice_version() {
-  echo "Updating Dockerfile and commiting to master..."
+  timestamp Updating Dockerfile and commiting to master...
   sed -i "/ENV\ ICE_VERSION/c\ENV\ ICE_VERSION\ $NEW_VERSION" ice/Dockerfile
   git commit ice/Dockerfile -m "Created new release: $NEW_VERSION"
   git push
 }
 
 create_release_tag() {
-  echo "Creating release tag..."
+  timestamp Creating release tag...
   sed -i "/ENV\ ICE_VERSION/c\ENV\ ICE_VERSION\ $NEW_VERSION" ice/Dockerfile
   git tag -m "New version of ICE" "$NEW_VERSION.0"
   git remote rm origin
